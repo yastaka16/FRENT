f7ea0bc47ba2c94ea6128573f42232df9aa4dc84
yastaka16 <yasutaka7pasio@gmail.com>
2016-09-15 21:11:22 +0900

    first commit

---
+++ FRENT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FRENT
+//
+//  Created by yasutaka on 2016/09/15.
+//  Copyright © 2016年 yasutaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
